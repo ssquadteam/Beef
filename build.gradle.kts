bf1cd21106d4df2d21435399f0dbb1eea8156db0
Fafnir <157227425+The-Fafnir@users.noreply.github.com>
2024-06-29 02:12:23 +0600

    Initial Release 1.21

---
+++ build.gradle.kts
@@ -0,0 +1,127 @@
+plugins {
+    java
+    `maven-publish`
+    id("io.papermc.paperweight.patcher") version "1.7.2-SNAPSHOT"
+}
+
+val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
+
+repositories {
+    mavenCentral()
+    maven(paperMavenPublicUrl) {
+        content { onlyForConfigurations(configurations.paperclip.name) }
+    }
+    maven("https://maven.nostal.ink/repository/maven-snapshots/")
+}
+
+dependencies {
+    remapper("net.fabricmc:tiny-remapper:0.10.3:fat")
+    decompiler("org.vineflower:vineflower:1.10.1")
+    paperclip("cn.dreeam:quantumleaper:1.0.0-SNAPSHOT")
+}
+
+allprojects {
+    apply(plugin = "java")
+    apply(plugin = "maven-publish")
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(21))
+        }
+    }
+}
+
+subprojects {
+    tasks.withType<JavaCompile> {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(21)
+    }
+    tasks.withType<Javadoc> {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    tasks.withType<ProcessResources> {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+
+    repositories {
+        mavenCentral()
+        maven(paperMavenPublicUrl)
+        maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
+        maven("https://repo.aikar.co/content/groups/aikar")
+        maven("https://repo.md-5.net/content/repositories/releases/")
+        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+        maven("https://jitpack.io")
+        maven("https://repo.codemc.io/repository/maven-public/")
+    }
+}
+
+paperweight {
+    serverProject.set(project(":leaf-server"))
+
+    remapRepo.set("https://maven.fabricmc.net/")
+    decompileRepo.set("https://maven.quiltmc.org/")
+
+    useStandardUpstream("Gale") {
+        url.set(github("Dreeam-qwq", "Gale"))
+        ref.set(providers.gradleProperty("galeCommit"))
+
+        withStandardPatcher {
+            apiSourceDirPath.set("gale-api")
+            serverSourceDirPath.set("gale-server")
+
+            apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
+            apiOutputDir.set(layout.projectDirectory.dir("Leaf-API"))
+
+            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
+            serverOutputDir.set(layout.projectDirectory.dir("Leaf-Server"))
+        }
+
+        patchTasks.register("generatedApi") {
+            isBareDirectory = true
+            upstreamDirPath = "paper-api-generator/generated"
+            patchDir = layout.projectDirectory.dir("patches/generated-api")
+            outputDir = layout.projectDirectory.dir("paper-api-generator/generated")
+        }
+    }
+}
+
+tasks.generateDevelopmentBundle {
+    apiCoordinates = "cn.dreeam.leaf:leaf-api"
+    libraryRepositories.set(
+        listOf(
+            "https://repo.maven.apache.org/maven2/",
+            paperMavenPublicUrl,
+        )
+    )
+}
+
+publishing {
+    if (project.providers.gradleProperty("publishDevBundle").isPresent) {
+        publications.create<MavenPublication>("devBundle") {
+            artifact(tasks.generateDevelopmentBundle) {
+                artifactId = "dev-bundle"
+            }
+        }
+    }
+}
+
+allprojects {
+    publishing {
+        repositories {
+            maven {
+                name = "leaf"
+                url = uri("https://maven.pkg.github.com/Winds-Studio/Leaf")
+
+                credentials.username = System.getenv("REPO_PASSWORD")
+                credentials.password = System.getenv("REPO_USER")
+            }
+
+            publications {
+                register<MavenPublication>("gpr") {
+                    from(components["java"])
+                }
+            }
+        }
+    }
+}
